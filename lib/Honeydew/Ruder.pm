1e9d47478d6e66b79676ae94df5f1db3a1c3985b
Daniel Gempesaw <gempesaw@gmail.com>
2016-11-28 13:30:33 -0500

    initial commit

---
+++ lib/Honeydew/Ruder.pm
@@ -0,0 +1,15 @@
+package Honeydew::Ruder;
+
+# ABSTRACT:
+use strict;
+use warnings;
+
+=for markdown [![Build Status](https://travis-ci.org/gempesaw/Honeydew-Ruder.svg?branch=master)](https://travis-ci.org/gempesaw/Honeydew-Ruder)
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=cut
+
+1;
