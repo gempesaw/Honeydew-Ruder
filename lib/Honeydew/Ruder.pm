358c8ba86fafb3a4722305cd253a67cdc8408381
Daniel Gempesaw <gempesaw@gmail.com>
2016-11-28 13:56:32 -0500

    Create dead simple ruder client api

--- lib/Honeydew/Ruder.pm
+++ lib/Honeydew/Ruder.pm
@@ -1,15 +1,71 @@
 package Honeydew::Ruder;
 
-# ABSTRACT:
+# ABSTRACT: Talk to the Ruder nodeJS service
 use strict;
 use warnings;
+use Moo;
+use Honeydew::Config;
+use LWP::UserAgent;
 
 =for markdown [![Build Status](https://travis-ci.org/gempesaw/Honeydew-Ruder.svg?branch=master)](https://travis-ci.org/gempesaw/Honeydew-Ruder)
 
 =head1 SYNOPSIS
 
+    my $req = Honeydew::Ruder->new('GET https://www.google.com');
+    my $res = $req->execute;
+    say $res;
+
 =head1 DESCRIPTION
 
+Use this module to interact with the Ruder NodeJS service. Instantiate
+an object by passing in a multiline string of an HTTP request and
+invoke L</execute> to get the result.
+
 =cut
+
+has request => (
+    is => 'ro',
+    required => 1
+);
+
+has ua => (
+    is => 'lazy',
+    default => sub { return LWP::UserAgent->new }
+);
+
+has config => (
+    is => 'lazy',
+    default => sub { return Honeydew::Config->instance }
+);
+
+around BUILDARGS => sub {
+    my ( $orig, $class, @args ) = @_;
+
+    return { request => $args[0] }
+      if @args == 1 && !ref $args[0];
+
+    return $class->$orig(@args);
+};
+
+sub execute {
+    my ($self, %args) = @_;
+
+    my $response = $self->ua->post(
+        $self->_ruder_addr,
+        data => $self->request
+    );
+
+    if ($args{debug}) {
+        return $response;
+    }
+    else {
+        return $response->content;
+    }
+}
+
+sub _ruder_addr {
+    my ($self) = @_;
+    return $self->config->{ruder}->{server};
+}
 
 1;
--- t/Honeydew-Ruder.t
+++ t/Honeydew-Ruder.t
@@ -1,5 +1,56 @@
 use strict;
 use warnings;
 use Test::Spec;
+use HTTP::Response;
+use Honeydew::Ruder;
+use Honeydew::Config;
+
+describe 'Ruder' => sub {
+    my ($req);
+
+    my $config = Honeydew::Config->instance( file => __FILE__ );
+    $config->{ruder}->{server} = 'localhost';
+
+    before each => sub {
+        $req = 'GET https://www.google.com';
+    };
+
+    it 'should not require a hash to new' => sub {
+        my $ruder = Honeydew::Ruder->new($req);
+        is($ruder->request, $req);
+    };
+
+    describe 'execution' => sub {
+        my ($ua_mock);
+        before each => sub {
+            $ua_mock = mock();
+            $ua_mock->expects('post')
+              ->with_deep('localhost', data => $req)
+              ->returns(HTTP::Response->new(200, undef, undef, 'response'));
+        };
+
+
+        it 'should send the request to the ruder server' => sub {
+            my $ruder = Honeydew::Ruder->new(
+                request => $req,
+                ua => $ua_mock
+            );
+
+            my $response = $ruder->execute;
+            is($response, 'response');
+        };
+
+        it 'should return the entire response for debugging' => sub {
+            my $ruder = Honeydew::Ruder->new(
+                request => $req,
+                ua => $ua_mock
+            );
+
+            my $response = $ruder->execute( debug => 1 );
+            is($response->content, 'response');
+
+        };
+    };
+};
 
 runtests;
